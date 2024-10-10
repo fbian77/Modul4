c311ca58064c4c7252d111aada08a5e4ba780ffb
fabianholzknecht <fabianholzknecht@tsn.at>
2024-10-10 13:39:42 +0200

    Wir haben die Klasse Export, ExportCSV und ExportJSN erstellt

---
+++ src/Export.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public abstract class Export {
+
+    public  abstract void ausgeben(ArrayList<Artikel> artikelliste);
+}
---
+++ src/ExportJSON.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class ExportJSON extends Export {
+    @Override
+    public void ausgeben(ArrayList<Artikel> artikelliste) {
+
+    }
+}
